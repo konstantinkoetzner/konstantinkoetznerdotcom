9b000224e9207f22c2dc10830e2eaf69284992c9
konstantinkoetzner <171531275+konstantinkoetzner@users.noreply.github.com>
2024-07-16 16:27:41 +0000

    Add initial server setup and index page template

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", indexHandler)
+
+	fmt.Println("Server is running on http://localhost:8080")
+	http.ListenAndServe(":8080", mux)
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("template/index.gohtml")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	err = t.Execute(w, nil)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
