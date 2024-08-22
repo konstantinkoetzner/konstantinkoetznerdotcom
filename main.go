fd61b6d8a8cdcc790067138659d4c592f0cc9ea0
konstantinkoetzner <171531275+konstantinkoetzner@users.noreply.github.com>
2024-08-22 16:45:52 +0000

    Add language selection functionality

--- main.go
+++ main.go
@@ -1,15 +1,18 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"html/template"
 	"net/http"
 	"path/filepath"
+	"time"
 )
 
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", indexHandler)
+	mux.HandleFunc("/language", languageHandler)
 	mux.HandleFunc("/css/", cssHandler)
 	mux.HandleFunc("/img/", imgHandler)
 
@@ -24,11 +27,53 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	err = t.Execute(w, nil)
+	lang := "en"
+
+	lc, err := r.Cookie("language")
+
+	if err != nil && !errors.Is(err, http.ErrNoCookie) {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	if lc != nil {
+		lang = lc.Value
+	}
+
+	fmt.Println(lc)
+	fmt.Println(lang)
+
+	err = t.Execute(w, lang)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
+}
+
+func languageHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	lang := r.FormValue("lang")
+
+	if lang != "en" && lang != "de" {
+		http.Error(w, "Invalid language", http.StatusBadRequest)
+		return
+	}
+
+	http.SetCookie(w, &http.Cookie{
+		Name:     "language",
+		Value:    lang,
+		Path:     "/",
+		MaxAge:   int(365 * 24 * time.Hour / time.Second),
+		HttpOnly: true,
+		Secure:   false,
+		SameSite: http.SameSiteLaxMode,
+	})
+
+	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
 
 func cssHandler(w http.ResponseWriter, r *http.Request) {
